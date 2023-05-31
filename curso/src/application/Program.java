090eac1b92a0486fc074351548adb90f697b3401
Marcos Vinicius Marques <marquesmarcosv@outlook.com>
2023-05-31 19:31:56 -0300

    aulas básicas
    primeiras aulas de programação em java.

---
+++ curso/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidade.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner(System.in);
+				
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+		System.out.println("Price: ");
+		double price = sc.nextDouble();
+		
+		Product product = new Product(name, price, 0);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
+		System.out.println();
+		System.out.println("Products data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProduct(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProduct(quantity);
+
+		System.out.println();
+		System.out.println("Products data: " + product);
+		
+		sc.close();
+	}
+
+}
